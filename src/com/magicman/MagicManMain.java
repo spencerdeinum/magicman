d61296beb2a5d44dce8fa4bd5feb47d6790c5656
Spencer Deinum <spencerdeinum@gmail.com>
2013-07-05 22:34:46 -0400

    Initial commit. Signed-off-by: Spencer Deinum <spencerdeinum@gmail.com>

---
+++ src/com/magicman/MagicManMain.java
@@ -0,0 +1,16 @@
+package com.magicman;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.magicman.R;
+
+public class MagicManMain extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
